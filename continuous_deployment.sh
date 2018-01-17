601d393e17c2f2c12f02367767d2aca6e3514fa3
Ori Hoch <ori@uumpa.com>
2018-01-17 13:07:29 +0200

    .travis

--- continuous_deployment.sh
+++ continuous_deployment.sh
@@ -3,10 +3,14 @@
 if \
     [ "${CONTINUOUS_DEPLOYMENT_OPS}" == "1" ]
 then
-    cd /pwd
     RES=0
-    ! ./cloudbuild.sh gcr.io/midbarrn/midburn-profiles-drupal:_ midbarrn midburn-profiles-drupal . && RES=1
-    ! ./cloudbuild.sh gcr.io/midbarrn/midburn-profiles-drupal-db:_ midbarrn midburn-profiles-drupal-db db && RES=1
+    cd /ops
+    ! ./helm_update_values.sh "${B64_UPDATE_VALUES}" "${HELM_UPDATE_COMMIT_MESSAGE}" "${K8S_OPS_GITHUB_REPO_TOKEN}" \
+                              "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" && RES=1
+
+    cd /pwd
+    ! ./cloudbuild.sh ${IMAGE_TAG} midbarrn midburn-profiles-drupal . && RES=1
+    ! ./cloudbuild.sh ${DB_IMAGE_TAG} midbarrn midburn-profiles-drupal-db db && RES=1
     exit $RES
 
 elif [ "${DEPLOY_ENVIRONMENT}" != "" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] &&\
@@ -16,14 +20,25 @@ then
     openssl aes-256-cbc -K $encrypted_9e4a6751a6cc_key -iv $encrypted_9e4a6751a6cc_iv -in ./k8s-ops-secret.json.enc -out ./secret-k8s-ops.json -d
     OPS_REPO_SLUG="Midburn/midburn-k8s"
     OPS_REPO_BRANCH="${OPS_REPO_BRANCH:-master}"
+    IMAGE_TAG="gcr.io/midbarrn/midburn-profiles-drupal:${TRAVIS_TAG:-$TRAVIS_COMMIT}"
+    DB_IMAGE_TAG="gcr.io/midbarrn/midburn-profiles-drupal-db:${TRAVIS_TAG:-$TRAVIS_COMMIT}"
+    echo "IMAGE_TAG=${IMAGE_TAG}"
+    echo "DB_IMAGE_TAG=${DB_IMAGE_TAG}"
+    B64_UPDATE_VALUES=`echo '{"profiles":{"image":"'${IMAGE_TAG}'", "dbImage":"'${DB_IMAGE_TAG}'"}}' | base64 -w0`
+    HELM_UPDATE_COMMIT_MESSAGE="update midburn profiles drupal images"
     wget https://raw.githubusercontent.com/OriHoch/sk8s-ops/master/run_docker_ops.sh
     chmod +x run_docker_ops.sh bin/continuous_deployment.sh
     ! ./run_docker_ops.sh "${DEPLOY_ENVIRONMENT}" "/pwd/continuous_deployment.sh" \
                           "orihoch/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "" "
                             -v `pwd`:/pwd
+                            -e B64_UPDATE_VALUES=${B64_UPDATE_VALUES}
+                            -e HELM_UPDATE_COMMIT_MESSAGE=${HELM_UPDATE_COMMIT_MESSAGE}
+                            -e K8S_OPS_GITHUB_REPO_TOKEN=${K8S_OPS_GITHUB_REPO_TOKEN}
                             -e OPS_REPO_SLUG=${OPS_REPO_SLUG}
                             -e OPS_REPO_BRANCH=${OPS_REPO_BRANCH}
                             -e CONTINUOUS_DEPLOYMENT_OPS=1
+                            -e IMAGE_TAG=${IMAGE_TAG}
+                            -e DB_IMAGE_TAG=${DB_IMAGE_TAG}
                           " \
         && echo 'failed to run docker ops' && exit 1
 fi
